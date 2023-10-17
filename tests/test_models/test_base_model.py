e1627439e35668d83294e42ada38bc387bd90a66
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-17 14:50:42 -0500

    added test case for save method

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
+"""Unit tests for BaseModel"""
 
+
+from models.base_model import BaseModel
 import unittest
-import models.base_model
+
 
 class TestBase_model(unittest.TestCase):
     def test_new(self):
+        """assert new item created with set values"""
         my_model = BaseModel()
         my_model.name = "My First Model"
         my_model.my_number = 89
-        self.assertTrue(my_model.name == 'My First Model')
+        self.assertTrue(my_model.name == "My First Model")
         self.assertTrue(my_model.my_number == 89)
+
+    def test_save(self):
+        model = BaseModel()
+        time_before_save = model.updated_at
+        model.save()
+        time_after_save = model.updated_at
+        self.assertTrue(time_before_save != time_after_save)
