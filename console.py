65c0c6f06f22c313a1d3489e33728455e138806f
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-19 14:01:51 -0500

    6. Console 0.0.1

--- console.py
+++ console.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python3
-'''The console for AirBnB clone
-'''
+"""The console for AirBnB clone
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg) -> bool:
+        """Exit the console"""
+        return True
+
+    def do_EOF(self, arg) -> bool:
+        """Exit the console"""
+        return True
+
+    def emptyline(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
