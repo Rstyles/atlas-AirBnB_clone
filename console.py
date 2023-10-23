06a70916e2d21722d5ece6377e659b57acc44c7c
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-23 12:00:35 -0500

    updated imports

--- console.py
+++ console.py
@@ -5,6 +5,12 @@ import cmd
 
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
 from models import storage
 
 
@@ -150,5 +156,6 @@ class HBNBCommand(cmd.Cmd):
                 return
         print("** no instance found **")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
