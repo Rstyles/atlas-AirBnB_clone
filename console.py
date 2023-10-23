57927519541c506bc21dc830148f37c8e6f6786e
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-23 11:56:03 -0500

    fixed error in console

--- console.py
+++ console.py
@@ -149,3 +149,6 @@ class HBNBCommand(cmd.Cmd):
                 storage.save()
                 return
         print("** no instance found **")
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
